8fb2cd37620bf63b2fa3afc5ee94e126aab4166e
ThreadDao <yufen.zong@zilliz.com>
2022-03-04 09:26:10 +0000

    Update locust config for pinecone

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,42 +1,26 @@
-#!/usr/bin/env python
+import random
+from locust import HttpUser, task
 
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+class PineconeUser(HttpUser):
 
-
-import uuid
-
-from datetime import datetime
-from locust import HttpLocust, TaskSet, task
-
-
-class MetricsTaskSet(TaskSet):
-    _deviceid = None
-
-    def on_start(self):
-        self._deviceid = str(uuid.uuid4())
-
-    @task(1)
-    def login(self):
-        self.client.post(
-            '/login', {"deviceid": self._deviceid})
-
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
-
-
-class MetricsLocust(HttpLocust):
-    task_set = MetricsTaskSet
+    @task
+    def query_task(self):
+        # index_name = "sift-128"
+        url = "https://latency-10m-2p-0d908f2.svc.us-west1-gcp.pinecone.io/query"
+        nq = 1
+        query_vec = []
+        for _ in range(nq):
+            values = [random.random() for _ in range(128)]
+            query_vec.append({"values": values})
+        data = {
+            "namespace": "",
+            "topK": 1,
+            "includeValues": False,
+            "includeMetadata": False,
+            "queries": query_vec
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "Api-Key": "d3cce957-2401-4c94-832a-6db8eee658e5"
+        }
+        self.client.post(url=url, headers=headers, json=data)
