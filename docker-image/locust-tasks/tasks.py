5a19abf5bf049bab2832494ae2d072796852f725
ThreadDao <yufen.zong@zilliz.com>
2022-08-25 16:04:22 +0800

    Update task file to test new
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,23 +1,26 @@
 import random
 from locust import HttpUser, task
 
+
 class PineconeUser(HttpUser):
 
     @task
     def query_task(self):
         # index_name = "sift-128"
-        url = "https://latency-10m-2p-0d908f2.svc.us-west1-gcp.pinecone.io/query"
+        # url = "https://latency-10m-2p-0d908f2.svc.us-west1-gcp.pinecone.io/query"
+        url = "https://sift-128-euclidean-61a3a8a.svc.us-west1-gcp.pinecone.io/query"
+        dim = 128
         nq = 1
         query_vec = []
         for _ in range(nq):
-            values = [random.random() for _ in range(128)]
+            values = [random.random() for _ in range(dim)]
             query_vec.append({"values": values})
         data = {
             "namespace": "",
             "topK": 1,
             "includeValues": False,
             "includeMetadata": False,
-            "queries": query_vec
+            "vector": query_vec
         }
         headers = {
             "Content-Type": "application/json",
