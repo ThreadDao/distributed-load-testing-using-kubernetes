81d38725d8eb6de70f61a6a2a54916b935b0c2b8
ThreadDao <yufen.zong@zilliz.com>
2023-04-13 16:52:38 +0800

    Locust tasks var from env
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,19 +1,16 @@
 import random
+import os
 from locust import HttpUser, task
+
+url = os.environ["url"]
+dim = os.environ["dim"]
+api_key = os.environ["api_key"]
 
 
 class PineconeUser(HttpUser):
 
     @task
     def query_task(self):
-        url = "https://sift-128-euclidean-61a3a8a.svc.us-west1-gcp.pinecone.io/query"
-        # url = "https://random-1m-768-0d908f2.svc.us-west1-gcp.pinecone.io/query"
-        dim = 768
-        # nq = 1
-        # query_vec = []
-        # for _ in range(nq):
-        #     values = [random.random() for _ in range(dim)]
-        #     query_vec.append({"values": values})
         query_vec = [random.random() for _ in range(dim)]
         data = {
             "namespace": "",
@@ -24,6 +21,6 @@ class PineconeUser(HttpUser):
         }
         headers = {
             "Content-Type": "application/json",
-            "Api-Key": "d3ccxxx-xxx-xxx-xxx-xxx58e5"
+            "Api-Key": api_key
         }
         self.client.post(url=url, headers=headers, json=data)
